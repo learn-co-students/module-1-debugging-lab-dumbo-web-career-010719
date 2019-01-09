c58a72507b42ed34e11b096a0c9be1e7d5dbfe4d
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-01-09 12:50:39 -0500

    Done.

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -1,11 +1,14 @@
 class GasStation
 
   attr_reader :brand, :unleaded_price
+  attr_writer :brand, :unleaded_price
 
   def initialize(brand, unleaded_price)
     @brand = brand
     @unleaded_price = unleaded_price
   end
+
+  
 
 end
 
